8a07880a10006bb376eec8b474ab4c47530642ab
tanhuynh4019 <98221135+tanhuynh4019@users.noreply.github.com>
2022-02-07 18:14:04 +0700

    fix

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
 const mongoose = require('mongoose');
 const express = require('express');
+const rateLimit = require('express-rate-limit');
 const dotenv = require('dotenv');
+const helmet = require('helmet');
 const cors = require('cors');
 const route = require('./routes/routes');
 
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
+
+
+//express-rate-limit
+const limiter = rateLimit({
+    // 15 minutes
+    windowMs: 15 * 60 * 1000,
+    // limit each IP to 100 requests per windowMs
+    max: 100
+});
+app.use(limiter);
+
+//helmet
+app.use(helmet())
 
 // plugin
 app.use(cors());
